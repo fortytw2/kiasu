5e15d29e618ecb748660692e185181f33edf7ffa
Ian Chiles <fortytw2@gmail.com>
2016-08-15 18:18:42 +0200

    add authorization tests + mocks

--- api/auth.go
+++ api/auth.go
@@ -1,18 +1,18 @@
 package api
 
 import (
+	"context"
 	"net/http"
 	"strings"
 
 	"github.com/fortytw2/kiasu"
-	"golang.org/x/net/context"
 )
 
 // Authenticate wraps any given handler in basic authentication
 // using Bearer tokens
 func Authenticate(us kiasu.UserStore, x http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		authHeader := w.Header().Get("Authorization")
+		authHeader := r.Header.Get("Authorization")
 		trimmed := strings.TrimLeft(authHeader, "Bearer ")
 		if trimmed == "" {
 			w.WriteHeader(http.StatusBadRequest)
@@ -24,10 +24,11 @@ func Authenticate(us kiasu.UserStore, x http.HandlerFunc) http.HandlerFunc {
 			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
+
 		// add the user + access_token to context
 		newCtx := context.WithValue(r.Context(), "user", user)
-		r.WithContext(context.WithValue(newCtx, "access_token", trimmed))
+		newR := r.WithContext(context.WithValue(newCtx, "access_token", trimmed))
 
-		x(w, r)
+		x(w, newR)
 	}
 }
--- mailer.go
+++ mailer.go
@@ -3,3 +3,7 @@ package kiasu
 // Mailer is an interface for sending emails - used primarily for user confirmations
 // but also send users notifications about "account expiry", etc
 type Mailer interface{}
+
+func FakeMailer() Mailer {
+	return nil
+}
