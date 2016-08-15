805138821bfd5a0d4bf55f0c6ced4dc20ddf6f6d
Ian Chiles <fortytw2@gmail.com>
2016-08-15 18:20:33 +0200

    golint comments

--- mailer.go
+++ mailer.go
@@ -4,6 +4,7 @@ package kiasu
 // but also send users notifications about "account expiry", etc
 type Mailer interface{}
 
+// FakeMailer returns a fake mailer suitable for using in tests / locally
 func FakeMailer() Mailer {
 	return nil
 }
