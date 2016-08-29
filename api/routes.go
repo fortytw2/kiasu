df81647053f43cb5b63ca57997ca79e181b5338d
Ian Chiles <fortytw2@gmail.com>
2016-08-29 20:38:11 +0200

    import chi logger + wrap protected routes

--- api/routes.go
+++ api/routes.go
@@ -12,7 +12,7 @@ import (
 // Routes returns all routes for the HTTP/JSON interface :)
 func Routes(l log.Logger, m kiasu.Mailer, ds kiasu.Store) http.Handler {
 	r := chi.NewRouter()
-	r.Use(middleware.Logger)
+	r.Use(logger(l))
 	r.Use(middleware.Recoverer)
 
 	r.Mount("/api/v1/users", userRoutes(l, m, ds))
@@ -23,11 +23,11 @@ func Routes(l log.Logger, m kiasu.Mailer, ds kiasu.Store) http.Handler {
 func userRoutes(l log.Logger, m kiasu.Mailer, ds kiasu.Store) http.Handler {
 	users := chi.NewRouter()
 
-	users.Get("/", UserProfile(l, ds).ServeHTTP)
-	users.Delete("/", DeactivateUser(l, ds).ServeHTTP)
+	users.Get("/", Authenticate(ds, UserProfile(l, ds)).ServeHTTP)
+	users.Delete("/", Authenticate(ds, DeactivateUser(l, ds)).ServeHTTP)
 
-	users.Get("/sessions", UserSessions(l, ds).ServeHTTP)
-	users.Delete("/sessions", Logout(l, ds).ServeHTTP)
+	users.Get("/sessions", Authenticate(ds, UserSessions(l, ds)).ServeHTTP)
+	users.Delete("/sessions", Authenticate(ds, Logout(l, ds)).ServeHTTP)
 
 	users.Get("/confirm", ConfirmToken(l, m, ds).ServeHTTP)
 	users.Post("/login", Login(l, m, ds).ServeHTTP)
